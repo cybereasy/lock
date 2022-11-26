d005881f8f3f57032b6837f3f77bfbda33c0e548
Cybereasy <118338725+mrcybereasy@users.noreply.github.com>
2022-11-26 18:53:17 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#bin/bash
+cp -r /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/home/lock
+mv /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc2
+cp -r /data/data/com.termux/files/home/lock/file/bash.bashrc /data/data/com.termux/files/usr/etc/
+pkg install toilet
+pkg install figlet
+pkg install termimage
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#bin/bash
+rm -rf  /data/data/com.termux/files/usr/etc/bash.bashrc
+mv /data/data/com.termux/files/usr/etc/bash.bashrc2 /data/data/com.termux/files/usr/etc/bash.bashrc
+mv /data/data/com.termux/files/home/lock/bash.bashrc /data/data/com.termux/files/usr/etc/
+
