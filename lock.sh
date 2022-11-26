59372e0c925e7cef479ec51d14c8f156f79c85d3
Cybereasy <118338725+mrcybereasy@users.noreply.github.com>
2022-11-26 19:32:12 +0530

    Add files via upload

---
+++ lock.sh
@@ -0,0 +1,18 @@
+#bin/bash
+echo "Made by cybereasy _________________________________"|lolcat
+echo "---------------------------------------------------"|lolcat
+toilet -f term -F border --gay Enter the password : |lolcat
+echo "---------------------------------------------------"|lolcat
+echo "___________________________________________________"|lolcat
+read a
+   if [ $a -eq 9990 ];then
+   cd $HOME
+   else
+echo $'\e[1;36m' X Wrong password X 
+echo " "
+termimage /data/data/com.termux/files/home/lock/file/lck.jpge
+sleep 3.0 
+clear
+  bash /data/data/com.termux/files/home/lock/file/lock.sh
+
+fi
